bec09bc57b12af0a058d4c9d4757be7da6ee035a
Marlon Mascarenhas <mmascarenhs@gmail.com>
2024-12-04 01:24:10 +0000

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import img1 from '/workspaces/clone-tabnews/pages/img/img1.jpg';
-
 function Home() {
     return (
         <>
-            <h1>Te amo, fica brava não S2</h1> 
-            <img src={img1} width="300" />
+            <h1>Te amo vivinha linda do meu coração, fica brava não S2</h1>
         </>
     );
 }
