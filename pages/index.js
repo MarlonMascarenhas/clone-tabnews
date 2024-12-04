c958c77a4bf263af808829c3938f2be103c6a2e8
Marlon Mascarenhas <mmascarenhs@gmail.com>
2024-12-04 01:17:24 +0000

    git init my project in web

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import img1 from '/workspaces/clone-tabnews/pages/img/img1.jpg';
+
+function Home() {
+    return (
+        <>
+            <h1>Te amo, fica brava não S2</h1> 
+            <img src={img1} width="300" />
+        </>
+    );
+}
+
+export default Home;
